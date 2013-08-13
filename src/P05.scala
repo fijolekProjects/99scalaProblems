6cc379e96644a5a018f90c688bdd37c67bcecc62
Michal Fijolek <mfi@touk.pl>
2013-08-13 23:26:02 +0200

    ex P08

--- src/P05.scala
+++ src/P05.scala
@@ -13,7 +13,7 @@ object P05 {
   def reverse2(xs: List[Int]): List[Int] = {
     xs.foldLeft(List(): List[Int])((processedElements, currentElement) => currentElement :: processedElements)
   }                                               //> reverse2: (xs: List[Int])List[Int]
-  
+   
   reverse2(List(1, 1, 2, 3, 5, 8))                //> res1: List[Int] = List(8, 5, 3, 2, 1, 1)
 
 }
