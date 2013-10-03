fea9dc378d6713a9236c31b3409644fbd221c36a
Michal Fijolek <mfi@touk.pl>
2013-10-04 00:14:04 +0200

    P13 modified

--- src/P13.scala
+++ src/P13.scala
@@ -2,12 +2,9 @@ object P13 {
   val list = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
                                                   //> list  : List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, '
                                                   //| e, 'e)
-  def encodeDirect1[A](xs: List[A]): List[(Int, A)] = {
-    if (xs.isEmpty) Nil
-    else {
-      val (one, rest) = xs.span(_ == xs.head)
-      (one.length, one.head) :: encodeDirect1(rest)
-    }
+  def encodeDirect1[A](xs: List[A]): List[(Int, A)] = xs.span(_ == xs.head) match {
+    case (Nil, Nil) => Nil
+    case (one, rest) => (one.length, one.head) :: encodeDirect1(rest)
   }                                               //> encodeDirect1: [A](xs: List[A])List[(Int, A)]
 
   encodeDirect1(list)                             //> res0: List[(Int, Symbol)] = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,
